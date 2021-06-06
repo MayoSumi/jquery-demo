acb94d2bdbcc0895c6aab896799350ca8871e0dc
Mayo Sumi <>
2021-06-07 00:43:09 +0900

    jqueryセットアップ

---
+++ backend/public/js/event.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*******************************!*\
+  !*** ./resources/js/event.js ***!
+  \*******************************/
+// $('document').ready(function () {
+//     $("p1").hover(function () {
+//     console.log('click')
+//     $(this).hide();
+// });
+// })
+window.onload = function () {
+  $("p1").hover(function () {
+    console.log('click');
+  });
+};
+/******/ })()
+;
---
+++ backend/resources/js/event.js
@@ -0,0 +1,12 @@
+// $('document').ready(function () {
+//     $("p1").hover(function () {
+//     console.log('click')
+//     $(this).hide();
+// });
+// })
+
+window.onload = function(){
+    $("p1").hover(function () {
+        console.log('click')
+    })
+};
